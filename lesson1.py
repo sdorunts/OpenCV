01dd5a727b75b98e7f8bb62dddc2c0e16586437b
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 02:56:31 +0300

    Add named window with autosize

--- lesson1.py
+++ lesson1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread("opencv_logo.png", -1) # Считываю изображение с прозрачным фоном (-1)
+cv2.namedWindow("image_logo", cv2.WINDOW_AUTOSIZE)
 cv2.imshow('image_logo', img) # Вывожу изображение в окне image_logo
 cv2.waitKey(0) # Бесконечно ожидаю нажатия кнопки
 cv2.destroyAllWindows()

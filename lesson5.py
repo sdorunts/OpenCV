c9b3a69536b961235bbd7ad7385831582bac37bd
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 21:07:42 +0300

    Change color gradient to gray grad

--- lesson5.py
+++ lesson5.py
@@ -6,9 +6,10 @@ cv2.namedWindow('frame')
 
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    framex = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=5)
-    framey = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5)
+    framex = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
+    framey = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
 
     cv2.imshow('frame', frame)
     cv2.imshow('framex', framex)

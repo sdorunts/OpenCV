08998a96a14660f14e9ba14b70f7352b2903b792
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 05:51:11 +0300

    Add trackbars

--- lesson4.py
+++ lesson4.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as np
 
+def nothing(x):
+    return
+
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('frame')
+cv2.namedWindow('trackbars')
+
+cv2.createTrackbar('HL', 'trackbars', 0, 180, nothing)
+cv2.createTrackbar('SL', 'trackbars', 0, 255, nothing)
+cv2.createTrackbar('VL', 'trackbars', 0, 255, nothing)
+cv2.createTrackbar('H', 'trackbars', 0, 180, nothing)
+cv2.createTrackbar('S', 'trackbars', 0, 255, nothing)
+cv2.createTrackbar('V', 'trackbars', 0, 255, nothing)
 
 while True:
     ret, frame = cap.read()

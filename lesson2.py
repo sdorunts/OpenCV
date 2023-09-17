2ec503280a8f6f6b517a3c4a187934573c67e295
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 03:48:42 +0300

    Draw rectangle

--- lesson2.py
+++ lesson2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 img = np.zeros((700, 700, 3), np.uint8)
 
-img = cv2.line(img, (0, 0), (600, 600), (255, 0, 0), 5)
+img = cv2.rectangle(img, (10, 10), (600, 600), (255, 0, 0), 5)
 
 cv2.namedWindow('image')
 cv2.imshow('image', img)

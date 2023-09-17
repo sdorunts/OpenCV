9f5f1a0df14e1d7514e736a439643c5dce7c43be
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 21:06:04 +0300

    Add x and y gradient with color

--- lesson5.py
+++ lesson5.py
@@ -7,7 +7,12 @@ cv2.namedWindow('frame')
 while True:
     ret, frame = cap.read()
 
+    framex = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=5)
+    framey = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5)
+
     cv2.imshow('frame', frame)
+    cv2.imshow('framex', framex)
+    cv2.imshow('framey', framey)
 
     if (cv2.waitKey(1) & 0xFF == 27):
         break

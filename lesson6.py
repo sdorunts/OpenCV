e1eddd975a8aa30592ca3e7114b7a85f67d77c0a
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 21:36:45 +0300

    Add draw contours

--- lesson6.py
+++ lesson6.py
@@ -6,9 +6,13 @@ cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
 
 while True:
     ret, frame = cap.read()
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edge = cv2.Canny(gray, 70, 110)
+    contours, h = cv2.findContours(edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
+    cv2.drawContours(frame, [contours[0]], -1, (0, 0, 255), 4)
     cv2.imshow('frame', frame)
     cv2.imshow('edge', edge)
 

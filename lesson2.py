03ff4382a56516a840c6c0db4a786c1e9507206b
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 03:57:09 +0300

    Filled the circle with color

--- lesson2.py
+++ lesson2.py
@@ -5,7 +5,7 @@ img = np.zeros((700, 700, 3), np.uint8)
 
 img = cv2.line(img, (10, 10), (600, 600), (100, 150, 50), 5)
 img = cv2.rectangle(img, (10, 10), (600, 600), (255, 0, 0), 5)
-img = cv2.circle(img, (500, 450), 100, (150, 50, 100), 3)
+img = cv2.circle(img, (500, 450), 100, (150, 50, 100), -1)
 
 cv2.namedWindow('image')
 cv2.imshow('image', img)

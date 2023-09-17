532139cd2f4ea833e3f9355be060f62ce6e72079
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 21:02:10 +0300

    Add video capture

---
+++ lesson5.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('frame')
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow('frame', frame)
+
+    if (cv2.waitKey(1) & 0xFF == 27):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

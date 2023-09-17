0a928e8eec1f2b2e397b76c77938816c891ea40e
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 05:54:13 +0300

    Get trackbars pos into variables

--- lesson4.py
+++ lesson4.py
@@ -7,7 +7,7 @@ def nothing(x):
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('frame')
-cv2.namedWindow('trackbars')
+cv2.namedWindow('trackbars', cv2.WINDOW_NORMAL)
 
 cv2.createTrackbar('HL', 'trackbars', 0, 180, nothing)
 cv2.createTrackbar('SL', 'trackbars', 0, 255, nothing)
@@ -19,6 +19,13 @@ cv2.createTrackbar('V', 'trackbars', 0, 255, nothing)
 while True:
     ret, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    hl  = cv2.getTrackbarPos('HL', 'trackbars')
+    sl  = cv2.getTrackbarPos('SL', 'trackbars')
+    vl  = cv2.getTrackbarPos('VL', 'trackbars')
+    h   = cv2.getTrackbarPos('H', 'trackbars')
+    s   = cv2.getTrackbarPos('S', 'trackbars')
+    v   = cv2.getTrackbarPos('V', 'trackbars')
 
     cv2.imshow('frame', frame)
     cv2.imshow('hsv_frame', hsv_frame)

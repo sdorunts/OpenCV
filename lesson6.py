839033ef361bb3aecf0792066ae08f8ad896ef50
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 21:27:31 +0300

    Add edges

--- lesson6.py
+++ lesson6.py
@@ -6,8 +6,11 @@ cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
 
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edge = cv2.Canny(gray, 70, 110)
 
     cv2.imshow('frame', frame)
+    cv2.imshow('edge', edge)
 
     if (cv2.waitKey(1) & 0xFF == 27):
         break

9530c8b693a48d11e183398baa03c16002c8168f
Shagen Dorunts <sdorunts@yandex.com>
2023-09-17 03:51:17 +0300

    Add line to rectangle

--- lesson2.py
+++ lesson2.py
@@ -3,6 +3,7 @@ import numpy as np
 
 img = np.zeros((700, 700, 3), np.uint8)
 
+img = cv2.line(img, (10, 10), (600, 600), (100, 150, 50), 5)
 img = cv2.rectangle(img, (10, 10), (600, 600), (255, 0, 0), 5)
 
 cv2.namedWindow('image')

2ec842dd06c2ed9c042278bde890a8e6477e5ab0
Shagen Dorunts <sdorunts@yandex.com>
2023-09-19 14:50:36 +0300

    Add bounding

--- lesson9.py
+++ lesson9.py
@@ -42,10 +42,12 @@ while True:
     contours, h = cv2.findContours(edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     contours    = sorted(contours, key=cv2.contourArea, reverse=True)
 
-    try:
-        cv2.drawContours(frame, [contours[0]], -1, (0, 0, 255), 4)
-    except Exception:
-        pass
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+
+        if (area > 300):
+            x, y, w, h = cv2.boundingRect(contours[i])
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
